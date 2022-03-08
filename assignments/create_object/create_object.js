b8416381501bf665eafd56ec28a96ac06201f930
pradeepku085 <pradeepku.085@gmail.com>
2022-03-08 14:34:08 +0530

    Assigment-4 CreateObject Submission

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,12 @@
 function CreateObject(arr) {
     // Write your code here
+    let obj = {};
+    for(let i=0; i<arr.length; i=i+2){
+    let k =arr[i];
+    let v = arr[i+1];
+    obj[k]= v;
+    }
+    return obj;
 }
 
 module.exports = CreateObject;
